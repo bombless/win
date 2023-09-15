71932e76c8e50a54f01198035edf147b165f6c66
向越可 <bombless@live.com>
2023-09-15 16:28:45 +0800

    Create button

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use windows::Win32::UI::WindowsAndMessaging::{
-    WNDCLASSA, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, MSG, WM_DESTROY, WM_PAINT, WINDOW_EX_STYLE, SHOW_WINDOW_CMD, HMENU,
+    WNDCLASSA, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, MSG, WM_DESTROY, WM_PAINT,
+    WINDOW_EX_STYLE, SHOW_WINDOW_CMD, HMENU, GWLP_HINSTANCE, BS_DEFPUSHBUTTON,
+    WS_TABSTOP, WS_VISIBLE, WS_CHILD, WINDOW_STYLE,
     CreateWindowExA, RegisterClassA, ShowWindow, GetMessageA, TranslateMessage,
-    DispatchMessageA, PostQuitMessage, DefWindowProcA
+    DispatchMessageA, PostQuitMessage, DefWindowProcA, GetWindowLongPtrA
 };
 use windows::Win32::System::LibraryLoader::GetModuleHandleA;
-use windows::Win32::Foundation::{LRESULT, LPARAM, WPARAM, HWND};
+use windows::Win32::Foundation::{LRESULT, LPARAM, WPARAM, HWND, HINSTANCE};
 use windows::core::PCSTR;
 use windows::Win32::Graphics::Gdi::{
     PAINTSTRUCT, BeginPaint, FillRect, EndPaint, COLOR_WINDOW, HBRUSH
@@ -40,6 +42,8 @@ fn main() {
         }
         ShowWindow(hwnd, SHOW_WINDOW_CMD(1));
 
+        create_window(hwnd, Class::Button, PCSTR("OK\0".as_ptr() as _), 10, 10, 100, 100);
+
         let mut msg = MSG::default();
 
         while GetMessageA(&mut msg as _, HWND(0), 0, 0).0 > 0 {
@@ -47,6 +51,34 @@ fn main() {
             DispatchMessageA(&mut msg as _);
         }
     }
+}
+enum Class {
+    Button,
+    Static,
+}
+impl Into<PCSTR> for Class {
+    fn into(self) -> PCSTR {
+        match self {
+            Class::Button => PCSTR("BUTTON\0".as_ptr() as _),
+            Class::Static => PCSTR("STATIC\0".as_ptr() as _),
+        }
+    }
+}
+unsafe fn create_window(parent: HWND, class: Class, text: PCSTR, x: i32, y: i32, w: i32, h: i32) -> HWND {
+    let class: PCSTR = class.into();
+    CreateWindowExA(
+        WINDOW_EX_STYLE(0),
+        class,  // Predefined class; Unicode assumed 
+        text,      // Button text 
+        WS_TABSTOP | WS_VISIBLE | WS_CHILD | WINDOW_STYLE(BS_DEFPUSHBUTTON as _),  // Styles 
+        x,         // x position 
+        y,         // y position 
+        w,        // Button width
+        h,        // Button height
+        parent,     // Parent window
+        HMENU(0),       // No menu.
+        HINSTANCE(GetWindowLongPtrA(parent, GWLP_HINSTANCE) as _),
+        None)
 }
 unsafe extern "system" fn wnd_proc(hwnd: HWND, param1: u32, param2: WPARAM, param3: LPARAM) -> LRESULT {
     match param1 {
